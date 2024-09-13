e17afbb03aa28697aa238d7d917d4d0557029982
amarNrddn <amarnrddn02@gmail.com>
2024-09-13 10:29:36 +0700

    update: sidebar destop

--- src/components/Navbar/NavDestop.jsx
+++ src/components/Navbar/NavDestop.jsx
@@ -1,55 +1,3 @@
-
-// import React, { useState } from 'react';
-// import { motion } from 'framer-motion';
-// import { navItems } from '../../contans/itemsNav';
-// import { useTheme } from '../../contexts/ThemeProvider';
-// import { themeDark, themeLight } from '../../contans/styles';
-// import Profile from '../atoms/Profile';
-// import ProfileSidebar from '../atoms/ProfileSidebar';
-
-// const NavDestop = () => {
-//    const { theme } = useTheme();
-//    const [isHovered, setIsHovered] = useState(false);
-
-//    return (
-//       <nav className="absolute left-0">
-//          <motion.div
-//             className={`h-screen ${theme === 'dark' ? `${themeDark.className}` : `${themeLight.className}`} shadow-md p-4`}
-//             onHoverStart={() => setIsHovered(true)}
-//             onHoverEnd={() => setIsHovered(false)}
-//             animate={{ width: isHovered ? 192 : 64 }} 
-//             transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-//             style={{ overflow: 'hidden' }}
-//          >
-//             <ul className="space-y-4">
-//                <ProfileSidebar />
-//                {navItems.map((item, i) => (
-//                   <li key={i} className="flex items-center space-x-4 cursor-pointer">
-//                      <span className="text-2xl">{item.icon}</span>
-//                      <motion.span
-//                         initial={{ opacity: 0 }}
-//                         animate={{ opacity: isHovered ? 1 : 0 }}
-//                         transition={{ duration: 0.5, ease: 'easeInOut' }}
-//                         className="whitespace-nowrap"
-//                      >
-//                         {item.navigation}
-//                      </motion.span>
-//                   </li>
-//                ))}
-//             </ul>
-//          </motion.div>
-//       </nav>
-//    );
-// };
-
-// export default NavDestop;
-
-
-
-
-
-
-
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { navItems } from '../../contans/itemsNav';
@@ -64,24 +12,25 @@ const NavDestop = () => {
    return (
       <nav className="absolute left-0">
          <motion.div
-            className={`h-screen ${theme === 'dark' ? `${themeDark.className}` : `${themeLight.className}`} shadow-md relative`}
+            className={`h-screen ${theme === 'dark' ? `${themeDark.className}` : `${themeLight.className}`} rounded-tr-lg rounded-br-lg shadow-md relative ${isHovered ? '' : 'pt-4'} `}
             onHoverStart={() => setIsHovered(true)}
             onHoverEnd={() => setIsHovered(false)}
-            animate={{ width: isHovered ? 192 : 64 }}
+            animate={{ width: isHovered ? 200 : 64 }}
             transition={{ type: 'spring', stiffness: 300, damping: 30 }}
             style={{ overflow: 'hidden' }}
          >
-            <div className=" absolute  flex items-center flex-col">
-               <div className="">
+            <div >
+               <div className={`${isHovered ? '' : 'flex flex-col items-center '}`}>
                   <ProfileSidebar isHovered={isHovered} />
                </div>
-               <ul className="space-y-4">
+               <ul className={`space-y-4 ${isHovered ? 'pl-4' : 'px-5'} pt-4`}>
                   {navItems.map((item, i) => (
                      <li key={i} className="flex items-center space-x-4 cursor-pointer">
                         <span className="text-xl">{item.icon}</span>
                         <motion.span
-                           initial={{ opacity: 0 }}
-                           animate={{ opacity: isHovered ? 1 : 0 }}
+                           initial={{ opacity: 0, x: 100 }}
+                           animate={{ opacity: isHovered ? 1 : 0, x: isHovered ? 0 : 100 }}
+                           di-hover
                            transition={{ duration: 0.5, ease: 'easeInOut' }}
                            className="whitespace-nowrap"
                         >
--- src/components/atoms/ProfileSidebar.jsx
+++ src/components/atoms/ProfileSidebar.jsx
@@ -1,19 +1,40 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { navprofil } from '../../assets'
+import { TbRosetteDiscountCheckFilled } from 'react-icons/tb'
 
-const ProfileSidebar = ({isHovered}) => {
+const ProfileSidebar = ({ isHovered }) => {
    return (
-      <div className="flex items-center space-x-4 ">
-         <img src={navprofil} alt="Profile" className="rounded-full w-10 h-10 object-cover" />
-         <motion.span
-            initial={{ opacity: 0 }}
-            animate={{ opacity: isHovered ? 1 : 0 }}
-            transition={{ duration: 0.5, ease: 'easeInOut' }}
-            className="whitespace-nowrap"
-         >
-            {/* Amar Nuruddin */}
-         </motion.span>
+      <div className="">
+         {isHovered ? (
+            <div className="">
+               <div className="pl-4 flex items-center ">
+                  <div className="mt-4 max-w-[100px] px-2 py-1 flex items-center gap-1 border-2 rounded-xl">
+                     <div className="w-2 h-2 bg-green-600 rounded-full animate-pulseDot"></div>
+                     <p className='text-xs'>Hire me.</p>
+                  </div>
+               </div>
+               <motion.div
+                  animate={{ width: isHovered ? 200 : 60 }}
+                  className="bg-neutral-50 flex flex-col justify-center items-center pt-5"
+               >
+                  <motion.img
+                     initial={{ opacity: 0, x: -30, filter: 'blur(10px)' }}
+                     animate={{ opacity: 1, x: 0, filter: 'blur(0px)' }}
+                     transition={{ duration: 2, ease: 'easeInOut' }}
+                     src={navprofil}
+                     alt="Profile"
+                     className="w-16 h-16 rounded-full object-cover"
+                  />
+                  <div className="mt-2 flex items-center">
+                     <span className="whitespace-nowrap text-lg font-semibold">Amar Nuruddin</span>
+                     <TbRosetteDiscountCheckFilled className='text-cyan-500 text-2xl ml-1' />
+                  </div>
+               </motion.div>
+            </div>
+         ) : (
+            <img src={navprofil} alt="Profile" className="rounded-full w-10 h-10 object-cover" />
+         )}
       </div>
    )
 }
